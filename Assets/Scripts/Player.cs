1d3910da87c9ba213c357bb3d52b4140eced9b86
Aslam Hadi <aslamhadi@gmail.com>
2014-02-17 11:06:59 +0800

    09 - Firing the Projectile

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public float PlayerSpeed;
+
+	// Use this for initialization
+    //void Start ()
+    //{
+    //    transform.position = new Vector3(2, 1, transform.position.z);	    
+    //}
+	
+	// Update is called once per frame
+	void Update ()
+	{        
+	    float amountToMove = Input.GetAxisRaw("Horizontal") * PlayerSpeed * Time.deltaTime;
+
+        //move the player
+        transform.Translate(Vector3.right * amountToMove);
+
+        //wrap
+        if(transform.position.x <= -7.5f) 
+            transform.position = new Vector3(7.4f, transform.position.y, transform.position.z);
+        else if (transform.position.x >= 7.5f)
+            transform.position = new Vector3(-7.4f, transform.position.y, transform.position.z);
+                
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public float ProjectileSpeed;
+    private Transform _projectileTransform;
+
+	// Use this for initialization
+    void Start()
+    {
+        _projectileTransform = transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float amountToMove = ProjectileSpeed * Time.deltaTime;
+
+        //move the projectile
+        _projectileTransform.Translate(Vector3.up * amountToMove);
+
+        //destroy the object
+        if(_projectileTransform.position.y >= 6.4f)
+            Destroy(gameObject);
+	}
+}
