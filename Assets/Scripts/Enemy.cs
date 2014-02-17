cdf265347fb95a167ca3b49ed461458450ed47cd
Aslam Hadi <aslamhadi@gmail.com>
2014-02-17 13:58:03 +0800

    12 - Creating the Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float MinSpeed;
+    public float MaxSpeed;
+
+    private float _currentSpeed;
+    private float _x, _y, _z;
+
+    // Use this for initialization
+    void Start()
+    {        
+        _y = 7.0f;
+        _z = 0.0f;
+        SetSpeedAndPositionX();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float amountToMove = _currentSpeed * Time.deltaTime;
+        transform.Translate(Vector3.down * amountToMove);
+
+        if (transform.position.y <= -5)
+        {
+            SetSpeedAndPositionX();            
+        }
+    }
+
+    void SetSpeedAndPositionX()
+    {
+        _currentSpeed = Random.Range(MinSpeed, MaxSpeed);
+        _x = Random.Range(-6f, 6f);
+        transform.position = new Vector3(_x, _y, _z);
+    }
+}
