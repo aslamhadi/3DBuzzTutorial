381c6c6ddbe7f9b5203a4bbe7fa8369987b6e85c
Aslam Hadi <aslamhadi@gmail.com>
2014-02-18 11:46:04 +0800

    19 - Player Destruction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
 
     public float PlayerSpeed;
     public GameObject ProjectilePrefab;
+    public GameObject ExplosionPrefab;
+
     public static int Score = 0;
     public static int Lives = 3;
 	
@@ -34,5 +36,20 @@ public class Player : MonoBehaviour
     {
         GUI.Label(new Rect(10, 10, 120, 20), "Score: " + Score);
         GUI.Label(new Rect(10, 30, 80, 20), "Lives: " + Lives);
+    }
+
+    void OnTriggerEnter(Collider otherObject)
+    {
+        if (otherObject.tag == "enemy")
+        {
+            Player.Lives--;
+
+            var enemy = (Enemy)otherObject.gameObject.GetComponent("Enemy");            
+            enemy.SetSpeedAndPosition();            
+            
+            //Debug.Log("We've been hit");
+            Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
