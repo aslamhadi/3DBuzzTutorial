09df3aaf2c3e85efe2f56838b20749f92d7608db
Aslam Hadi <aslamhadi@gmail.com>
2014-02-18 14:18:44 +0800

    21 - Setting Up Our Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    private const string InstructionText = "Instruction:\nPress Left and Right arrow to move.\nPress Spacebar to fire";
+    private const string StartGame = "Start Game";
+    private const int ButtonWidth = 200;
+    private const int ButtonHeight = 50;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 250, 200), InstructionText);
+        if (GUI.Button(new Rect((Screen.width/2) - (ButtonWidth/2),
+            (Screen.height/2) - (ButtonHeight/2),
+            ButtonWidth, ButtonHeight), StartGame))
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
